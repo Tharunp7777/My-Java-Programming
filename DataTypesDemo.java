6951a93961b199efe456bb0abca4d3a7edaf51ca
Tharun Derangula <43755742+Tharunp7777@users.noreply.github.com>
2021-07-04 14:53:14 +0530

    Day 1

---
+++ DataTypesDemo.java
@@ -0,0 +1,20 @@
+
+public class DataTypesDemo {
+
+	public static void main(String[] args) {
+	
+		int a=100; //int type of data
+		double d=15.5;
+		char c='A';
+		boolean b=false;
+		
+		String s="welcome";
+
+		System.out.println(a);
+		System.out.println(d);
+		System.out.println(c);
+		System.out.println(b);
+		System.out.println(s);
+	}
+
+}
---
+++ FirstJavaProgram.java
@@ -0,0 +1,13 @@
+public class FirstJavaProgram 
+{
+
+	public static void main(String args[])
+	{
+	
+		System.out.println("Hello world..welcome to java training...");
+		System.out.println("Selenium training....");
+	}
+
+	
+	
+}
